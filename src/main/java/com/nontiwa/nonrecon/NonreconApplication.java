a1a1306a29f32012e92915fb122fb9ed349da414
DESKTOP-JU0EHB2\Nontiwa <nonh459@gmail.com>
2025-11-28 04:17:58 +0700

    first commit

---
+++ src/main/java/com/nontiwa/nonrecon/NonreconApplication.java
@@ -0,0 +1,13 @@
+package com.nontiwa.nonrecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonreconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonreconApplication.class, args);
+	}
+
+}
