4c1d57415a04925f5ac9a742898fba226550b992
DESKTOP-JU0EHB2\Nontiwa <nonh459@gmail.com>
2025-12-22 22:45:02 +0700

    comment out SalesReportListener so i can make job run successfully first

--- src/main/java/com/nontiwa/nonrecon/dailysalesreport/SalesReportListener.java
+++ src/main/java/com/nontiwa/nonrecon/dailysalesreport/SalesReportListener.java
@@ -1,27 +1,31 @@
 package com.nontiwa.nonrecon.dailysalesreport;
 
 import lombok.NonNull;
+import lombok.SneakyThrows;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
-@Component
-public class SalesReportListener implements StepExecutionListener {
-
-    @Autowired
-    private SalesAggregator aggregator;
-
-    @Autowired
-    private JdbcBatchItemWriter<SalesSummary> writer;
-
-    @Override
-    public ExitStatus afterStep(@NonNull StepExecution stepExecution) {
-        writer.write(new ArrayList<>(aggregator.getSummaries()));
-        return ExitStatus.COMPLETED;
-    }
-}
+//@Component
+//public class SalesReportListener implements StepExecutionListener {
+//
+//    @Autowired
+//    private SalesAggregator aggregator;
+//
+//    @Autowired
+//    private JdbcBatchItemWriter<SalesSummary> writer;
+//
+//    @SneakyThrows
+//    @Override
+//    public ExitStatus afterStep(@NonNull StepExecution stepExecution) {
+////        writer.write(new ArrayList<>(aggregator.getSummaries()));
+//        writer.write(new Chunk<SalesSummary>(new ArrayList<>(aggregator.getSummaries())));
+//        return ExitStatus.COMPLETED;
+//    }
+//}
